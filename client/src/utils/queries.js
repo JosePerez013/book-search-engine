58ef09a016bbf0c50c7c275a732082232f12c390
JosePerez013 <joseperez951@icloud.com>
2023-08-14 21:12:44 -0700

    removed RESTful routes in exchange for gql routes and mutations

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+import { gql } from "@apollo/client";
+
+export const GET_ME = gql`
+  query MeQuery {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        authors
+        bookId
+        description
+        image
+        link
+        title
+      }
+      bookCount
+    }
+  }
+`;
